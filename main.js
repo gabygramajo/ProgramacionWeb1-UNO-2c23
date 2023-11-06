85eb3bc5d187c18362536df860d7b1fe66b32dd1
gabriel <ggabrieldeveloper@gmail.com>
2023-11-06 12:17:01 -0300

    add ajax & jquery

--- main.js
+++ main.js
@@ -1,119 +1,131 @@
-const btnNivelFacil = document.getElementById("facil");
-const btnNivelMedio = document.getElementById("medio");
-const btnNivelDificil = document.getElementById("dificil");
+const easyLvl = document.getElementById("facil");
+const midLvl = document.getElementById("medio");
+const hardLvl = document.getElementById("dificil");
 const nickName = document.getElementById("nickname");
-const btnIngresarNickname = document.getElementById("ingresarNickname");
-imgBandera = document.getElementById("imgBandera");
+const nicknameInitial = document.getElementById("nicknameInitial");
+const entryPoint = document.getElementById("iniciarNickname");
+const resetNickname = document.getElementById("ingresarNickname");
+const optionsBtn = document.querySelectorAll(".opciones button")
+const globalContainer = document.getElementById("global-container");
+const modal = document.getElementById("ventana-modal");
+const msg = document.getElementById("mensaje");
 
 
-const BBDD_banderas = {
+const BBDD_countries = {
   facil : {
-    bandera1: {
-      nombre: "uruguay",
-      opciones: ["Argentina", "Honduras", "Escocia", "Uruguay"] 
+    country1: {
+      name: "uruguay",
+      op_correct: "uruguay",
+      options: ["argentina", "honduras", "escocia", "uruguay"] 
     },
-    bandera2: {
-      nombre: "colombia",
-      opciones: ["", "", "", ""]
+    country2: {
+      name: "colombia",
+      op_correct: "colombia",
+      options: ["hoduras", "colombia", "ucrania", "nigeria"]
     },
-    bandera3: {
-      nombre: "chile",
-      opciones: ["", "", "", ""] 
+    country3: {
+      name: "chile",
+      op_correct: "chile",
+      options: ["chile", "sudán", "venezuela", "canadá"] 
     },
-    bandera4: {
-      nombre: "brasil",
-      opciones: ["", "", "", ""] 
+    country4: {
+      name: "brasil",
+      op_correct: "brasil",
+      options: ["nigeria", "francia", "brasil", "rusia"] 
     },
-    bandera5: {
-      nombre: "peru",
-      opciones: ["", "", "", ""] 
+    country5: {
+      name: "peru",
+      op_correct: "peru",
+      options: ["nicaragua", "yemen", "méxico", "peru"] 
     },
   },
   medio : {
-    bandera1: {
-      nombre: "uruguay",
-      opciones: ["Argentina", "Honduras", "Escocia", "Uruguay"] 
+    country1: {
+      name: "canada",
+      op_correct: "canada",
+      options: ["ecuador", "canada", "escocia", "rusia"] 
     },
-    bandera2: {
-      nombre: "colombia",
-      opciones: ["", "", "", ""]
+    country2: {
+      name: "france",
+      op_correct: "francia",
+      options: ["portugal", "chipre", "francia", "camerú"]
     },
-    bandera3: {
-      nombre: "chile",
-      opciones: ["", "", "", ""] 
+    country3: {
+      name: "united kingdom",
+      op_correct: "reino unido",
+      options: ["reino unido", "bolivia", "costa rica", "noruega"] 
     },
-    bandera4: {
-      nombre: "brasil",
-      opciones: ["", "", "", ""] 
+    country4: {
+      name: "russia",
+      op_correct: "rusia",
+      options: ["taiwan", "rusia", "rumania", "españa"] 
     },
-    bandera5: {
-      nombre: "peru",
-      opciones: ["", "", "", ""] 
+    country5: {
+      name: "italy",
+      op_correct: "italia",
+      options: ["malasia", "mexico", "irlanda", "italia"] 
     },
   },
   dificil : {
-    bandera1: {
-      nombre: "uruguay",
-      opciones: ["Argentina", "Honduras", "Escocia", "Uruguay"] 
+    country1: {
+      name: "nigeria",
+      op_correct: "nigeria",
+      options: ["nigeria", "sudán", "jamaica", "haití"] 
     },
-    bandera2: {
-      nombre: "colombia",
-      opciones: ["", "", "", ""]
+    country2: {
+      name: "portugal",
+      op_correct: "portugal",
+      options: ["reino unido", "italia", "portugal", "china"]
     },
-    bandera3: {
-      nombre: "chile",
-      opciones: ["", "", "", ""] 
+    country3: {
+      name: "switzerland",
+      op_correct: "suiza",
+      options: ["suiza", "canada", "autria", "polonia"] 
     },
-    bandera4: {
-      nombre: "brasil",
-      opciones: ["", "", "", ""] 
+    country4: {
+      name: "greece",
+      op_correct: "grecia",
+      options: ["paises bajos", "irlanda", "grecia", "suecia"] 
     },
-    bandera5: {
-      nombre: "peru",
-      opciones: ["", "", "", ""] 
+    country5: {
+      name: "estonia",
+      op_correct: "estonia",
+      options: ["eslovenia", "estonia", "serbia", "lituania"] 
     },
   }
 }
 
-const vidas = ["❤", "❤", "❤"];
-const BBDD_jugadores = [];
-const BanderasDelNivel = [];
+let lives = ["❤", "❤", "❤"];
+const BBDD_players = [];
+let countriesOfLvl = [];
 
 
-const crearJugador = (nickname) => {
-  const jugador = {
-    nombre : nickname,
-    puntaje: 0 
+const createPlayer = (nickname) => {
+  const player = {
+    name : nickname,
+    score: 0 
   }
-  BBDD_jugadores.push(jugador);
-}
-
-function actualizarBandera() {
-  imgBandera = banderas.nivelFacil.bandera2.src
+  BBDD_players.push(player);
 }
 
 
-function validarJugadorExistente(nuevoNickname) {
-
-  //const nicknameExiste = BBDD_jugadores.some((e) => e.nombre == nuevoNickname);
-
-  return BBDD_jugadores.some((e) => e.nombre == nuevoNickname);
+function validateExistingPlayer(newNickname) {
+  return BBDD_players.some((e) => e.name == newNickname);
 }
 
-function desactivarCampoObligatorio() {
-  nickName.classList.remove("obligatorio");
+function desactivateRequiredField() {
+  nicknameInitial.classList.remove("obligatorio");
 }
 
-function validarJugador(jugadorNickName) {
+function validatePlayer(playerNickName) {
 
-  const longitudValida = jugadorNickName.length >= 4 && jugadorNickName.length <= 8;
+  const longitudValida = playerNickName.length >= 4 && playerNickName.length <= 8;
 
-  if(longitudValida && !validarJugadorExistente(jugadorNickName) ) {
-    alert("jugadorNickName agregado")
-    crearJugador(jugadorNickName);
+  if(longitudValida && !validateExistingPlayer(playerNickName) ) {
+    createPlayer(playerNickName);
 
-    if(nickName.classList.contains("obligatorio")) 
-      desactivarCampoObligatorio();
+    if(nicknameInitial.classList.contains("obligatorio")) 
+      desactivateRequiredField();
 
   } else 
       alert("Nickname no válido o ya existente")
@@ -124,154 +136,129 @@ function desactivarPeventDefault(event) {
   event.preventDefault();
 }
 
-function ingresarNuevoJugador(e) {
+function resetGame(e) {
   desactivarPeventDefault(e);
 
-  validarJugador(nickName.value.trim());
+  let value = confirm("¿Estás seguro que deseas reiniciar el juego?");
+
+  if(value)
+    location.reload()
 }
 
-function activarPantalla() {
-  const pantalla = document.getElementById("pantalla")
-  const ventanaModal = document.getElementById("ventana-modal")
+function intialPlayer(e) {
+  desactivarPeventDefault(e);
 
-  if(pantalla.classList.contains("desactivar")) {
-    pantalla.classList.remove("desactivar");  
-    ventanaModal.classList.add("desactivar");
-  } 
-  else {
-    pantalla.classList.add("desactivar");
-    ventanaModal.classList.remove("desactivar");
-  }
+  validatePlayer(nicknameInitial.value.trim());
+  nickName.value = "";
 }
 
-function activarNivelDeJuego(e) {
+function activateDisplay() {
 
-  const pantalla = document.getElementById("pantalla");
-  const ventanaModal = document.getElementById("ventana-modal");
+  globalContainer.classList.remove("desactivar");  
+  modal.classList.add("desactivar");
 
-  // if (BBDD_jugadores.length >= 1)
-  //   confirm("")
+}
 
-  if(pantalla.classList.contains("desactivar")) {
-    pantalla.classList.remove("desactivar");  
-    ventanaModal.classList.add("desactivar");
+function activateGameLevel(e) {
+
+  console.log("lvl: ",e.target.id);
+
+  if(globalContainer.classList.contains("desactivar")) {
+    activateDisplay();
   } else {
     confirm("Esta acción reiniciará el juego.")
-    // pantalla.classList.add("desactivar");
-    // ventanaModal.classList.remove("desactivar");
+  }
+
+  countriesOfLvl = Object.values(BBDD_countries[e.target.id]);
+
+  const country = countriesOfLvl[0].name;
+  updateOptions(countriesOfLvl[0].options);
+  requestApi(country);
+}
+
+function startGame(e) {
+
+  if(BBDD_players.length == 0)
+    alert("Debes Ingersar tu nickname y luego alegir el nivel")
+  else 
+    activateGameLevel(e);
+}
+// jquery & ajax
+
+function requestApi(country){
+  
+  $.ajax({
+    url: `https://restcountries.com/v3.1/name/${country}`
+  })
+  .done(getCountryInfo)
+  .fail(requestError);
+}
+
+function requestError() {
+  alert("Se produjo un error");
+}
+
+function getCountryInfo(datos){
+
+  let data = JSON.parse(JSON.stringify(datos))[0];
+
+  let flag = data.flags.svg;
+  let flag_alt = data.flags.alt;
+
+  $('#imgBandera' ).attr('src', flag);
+  $('#imgBandera' ).attr('alt', flag_alt);
+}
+
+function updateOptions (options) {
+  console.log(options);
+  optionsBtn.forEach((btn, i) => { 
+    if( btn.classList.contains("falso"))
+      btn.classList.remove("falso");
+
+    btn.innerText = options[i];
+  })
+}
+
+function nextCountry (op) {
+
+  const quesitonCounter = document.querySelector(".contador-de-preguntas");
+
+  if(cont < countriesOfLvl.length && (op.innerText == countriesOfLvl[cont - 1].op_correct)) {
+    op.classList.add("correcto");
+    msg.innerText = "Opcion Correcta!"
+
+    setTimeout(() => {
+      requestApi(countriesOfLvl[cont].name);
+      updateOptions(countriesOfLvl[cont].options);
+      cont++;
+      quesitonCounter.innerText = `Pregunta ${cont} de 5.`;
+      op.classList.remove("correcto");
+      msg.innerText = "";
+    }, "1500");
+    
+  } else {
+    msg.innerText = "Opcion incorrecta!";
+    op.classList.add("falso");
+    console.log(op);
   }
 }
 
-function comenzarJuego(e) {
-  // if(nickName.classList.contains("obligatorio"))
-  if(BBDD_jugadores.length == 0)
-    alert("Debes Ingersar tu nickname y luego alegir el nivel")
-  else 
-    activarNivelDeJuego(e);
+function selectedOption(op) {
+  nextCountry(op.target);
 }
+
+
+// jquery events
+let cont = 1;
+$('.op-a').click(selectedOption);
+$('.op-b').click(selectedOption);
+$('.op-c').click(selectedOption);
+$('.op-d').click(selectedOption);
 
 // eventos
 
-btnNivelFacil.addEventListener("click", comenzarJuego);
-btnNivelMedio.addEventListener("click", comenzarJuego);
-btnNivelDificil.addEventListener("click", comenzarJuego);
-btnIngresarNickname.addEventListener("click", ingresarNuevoJugador);
-
-// const BBDD_banderas = {
-//   facil : {
-//     bandera1: {
-//       nombre: "uruguay",
-//       src: "./images/uruguay.svg",
-//       descripcion: "Bandera de celeste y blanca",
-//       opciones: ["Argentina", "Honduras", "Escocia", "Uruguay"] 
-//     },
-//     bandera2: {
-//       src: "./images/canada.svg",
-//       descripcion: "",
-//       nombre: "",
-//       opciones: [] 
-//     },
-//     bandera3: {
-//       src: "./images/",
-//       descripcion: "",
-//       nombre: "",
-//       opciones: [] 
-//     },
-//     bandera4: {
-//       src: "./images/",
-//       descripcion: "",
-//       nombre: "",
-//       opciones: [] 
-//     },
-//     bandera5: {
-//       src: "./images/",
-//       descripcion: "",
-//       nombre: "",
-//       opciones: [] 
-//     },
-//   },
-//   medio : {
-//     bandera1: {
-//       src: "./images/",
-//       descripcion: "",
-//       nombre: "",
-//       opciones: [] 
-//     },
-//     bandera2: {
-//       src: "./images/",
-//       descripcion: "",
-//       nombre: "",
-//       opciones: [] 
-//     },
-//     bandera3: {
-//       src: "./images/",
-//       descripcion: "",
-//       nombre: "",
-//       opciones: [] 
-//     },
-//     bandera4: {
-//       src: "./images/",
-//       descripcion: "",
-//       nombre: "",
-//       opciones: [] 
-//     },
-//     bandera5: {
-//       src: "./images/",
-//       descripcion: "",
-//       nombre: "",
-//       opciones: [] 
-//     },
-//   },
-//   dificil : {
-//     bandera1: {
-//       src: "./images/",
-//       descripcion: "",
-//       nombre: "",
-//       opciones: [] 
-//     },
-//     bandera2: {
-//       src: "./images/",
-//       descripcion: "",
-//       nombre: "",
-//       opciones: [] 
-//     },
-//     bandera3: {
-//       src: "./images/",
-//       descripcion: "",
-//       nombre: "",
-//       opciones: [] 
-//     },
-//     bandera4: {
-//       src: "./images/",
-//       descripcion: "",
-//       nombre: "",
-//       opciones: [] 
-//     },
-//     bandera5: {
-//       src: "./images/",
-//       descripcion: "",
-//       nombre: "",
-//       opciones: [] 
-//     },
-//   }
-// }
+easyLvl.addEventListener("click", startGame);
+midLvl.addEventListener("click", startGame);
+hardLvl.addEventListener("click", startGame);
+entryPoint.addEventListener("click", intialPlayer);
+resetNickname.addEventListener("click", resetGame);
